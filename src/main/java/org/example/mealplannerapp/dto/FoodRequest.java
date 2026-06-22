ef895b8b93f0b6f4539aeb61eb9e59af5bfa93a9
ChristosBodinas <chr.bodinas@gmail.com>
2026-06-22 22:35:10 +0300

    Finished unit tests for the FoodService class. Added handler method for MethodArgumentNotValidException.

--- src/main/java/org/example/mealplannerapp/dto/FoodRequest.java
+++ src/main/java/org/example/mealplannerapp/dto/FoodRequest.java
@@ -4,15 +4,36 @@ import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.Length;
 
 public record FoodRequest(
-        @NotBlank @Length(max = 45) String name,
-        @Length(max = 45) String brand,
-        @PositiveOrZero double calories100g,
-        @PositiveOrZero double protein100g,
-        @PositiveOrZero double carbs100g,
-        @PositiveOrZero double fat100g,
-        @PositiveOrZero double fiber100g,
-        @PositiveOrZero double purchasePrice,
-        @Positive double purchaseWeight,
-        @DecimalMin(value = "0.0", inclusive = false) @DecimalMax(value = "1.0", inclusive = true) double edibleRatio
+        @NotBlank(message = "Name cannot be blank.")
+        @Length(max = 45, message = "Name cannot be longer than 45 characters.")
+        String name,
+
+        @Length(max = 45, message = "Brand cannot be longer than 45 characters.")
+        String brand,
+
+        @PositiveOrZero(message = "Calories cannot be a negative number.")
+        double calories100g,
+
+        @PositiveOrZero(message = "Protein cannot be a negative number.")
+        double protein100g,
+
+        @PositiveOrZero(message = "Carbs cannot be a negative number.")
+        double carbs100g,
+
+        @PositiveOrZero(message = "Fat cannot be a negative number.")
+        double fat100g,
+
+        @PositiveOrZero(message = "Fiber cannot be a negative number.")
+        double fiber100g,
+
+        @PositiveOrZero(message = "Purchase price cannot be a negative number.")
+        double purchasePrice,
+
+        @Positive (message = "Purchase weight must be a positive number.")
+        double purchaseWeight,
+
+        @DecimalMin(value = "0.0", inclusive = false, message = "Edible ratio cannot be less than 0%.")
+        @DecimalMax(value = "1.0", inclusive = true, message = "Edible ratio cannot be less than 100%.")
+        double edibleRatio
 ) {
 }
---
+++ src/test/java/org/example/mealplannerapp/service/FoodServiceUnitTests.java
@@ -0,0 +1,291 @@
+package org.example.mealplannerapp.service;
+
+import org.example.mealplannerapp.dto.FoodRequest;
+import org.example.mealplannerapp.dto.FoodResponse;
+import org.example.mealplannerapp.entity.Food;
+import org.example.mealplannerapp.entity.User;
+import org.example.mealplannerapp.exception.ResourceNotFoundException;
+import org.example.mealplannerapp.exception.ResourceNotOwnedException;
+import org.example.mealplannerapp.mapper.FoodMapper;
+import org.example.mealplannerapp.repository.FoodRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class FoodServiceUnitTests {
+
+    @Mock
+    private FoodRepository foodRepository;
+    @Mock private FoodMapper foodMapper;
+    
+    @InjectMocks
+    private FoodService foodService;
+
+    private final double purchasePrice = 7.8;
+    private final double purchaseWeight = 0.4;
+    private final double edibleRatio = 0.9;
+
+    private FoodRequest defaultFoodRequest() {
+        return new FoodRequest(
+            "Fake Food", "Fake Brand", // name, brand
+            97.0, 12.0, 37.5, 4.5, 6.0, // calories100g, protein100g, carbs100g, fat100g, fiber100g
+            7.8, 0.4, 0.9); // purchasePrice, purchaseWeight, edibleRatio
+    }
+
+    private FoodResponse defaultFoodResponse() {
+        return new FoodResponse(
+            1L, "Fake Food", "Fake Brand", // id, name, brand
+            97.0, 12.0, 37.5, 4.5, 6.0, // calories100g, protein100g, carbs100g, fat100g, fiber100g
+            7.8 / (0.4 * 0.9)); // price100g
+    }
+
+    private FoodResponse alternateFoodResponse() {
+        return new FoodResponse(
+                2L, "Made-Up Meal", "Fake Brand", // id, name, brand
+                97.0, 12.0, 37.5, 4.5, 6.0, // calories100g, protein100g, carbs100g, fat100g, fiber100g
+                7.8 / (0.4 * 0.9)); // price100g
+    }
+
+    @Test
+    void createNewFood_happyFlow() {
+        // Arrange
+        User user = new User();
+        FoodRequest request = defaultFoodRequest();
+
+        Food mappedFood = new Food();
+        when(foodMapper.createFromRequest(request)).thenReturn(mappedFood);
+
+        Food savedFood = new Food();
+        when(foodRepository.save(mappedFood)).thenReturn(savedFood);
+
+        FoodResponse expectedResponse = defaultFoodResponse();
+        when(foodMapper.generateResponse(savedFood)).thenReturn(expectedResponse);
+
+        // Act
+        FoodResponse actualResponse = foodService.createNewFood(user, request);
+
+        // Assert
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+        assertThat(mappedFood.getUser()).isEqualTo(user);
+        assertThat(mappedFood.getPrice100g()).isEqualTo(
+            request.purchasePrice() / (request.purchaseWeight() * request.edibleRatio()));
+    }
+
+    @Test
+    void updateExistingFood_happyFlow() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        Food existingFood = new Food();
+        existingFood.setUser(user);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        FoodRequest request = defaultFoodRequest();
+
+        Food savedFood = new Food();
+        when(foodRepository.save(existingFood)).thenReturn(savedFood);
+
+        FoodResponse expectedResponse = defaultFoodResponse();
+        when(foodMapper.generateResponse(savedFood)).thenReturn(expectedResponse);
+
+        // Act
+        FoodResponse actualResponse = foodService.updateExistingFood(user, 99L, request);
+
+        // Assert
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+        assertThat(existingFood.getPrice100g()).isEqualTo(
+            request.purchasePrice() / (request.purchaseWeight() * request.edibleRatio()));
+    }
+
+    @Test
+    void updateExistingFood_foodNotFound() {
+        // Arrange
+        User user = new User();
+        FoodRequest request = defaultFoodRequest();
+
+        when(foodRepository.findById(99L)).thenReturn(Optional.empty());
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.updateExistingFood(user, 99L, request))
+            .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void updateExistingFood_foodNotOwned() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        User owner = mock(User.class);
+        when(owner.getId()).thenReturn(2L);
+
+        FoodRequest request = defaultFoodRequest();
+
+        Food existingFood = new Food();
+        existingFood.setUser(owner);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.updateExistingFood(user, 99L, request))
+                .isInstanceOf(ResourceNotOwnedException.class);
+    }
+
+    @Test
+    void retrieveFoodById_happyFlow() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        Food existingFood = new Food();
+        existingFood.setUser(user);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        FoodResponse expectedResponse = defaultFoodResponse();
+        when(foodMapper.generateResponse(existingFood)).thenReturn(expectedResponse);
+
+        // Act
+        FoodResponse actualResponse = foodService.retrieveFoodById(user, 99L);
+
+        // Assert
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+    }
+
+    @Test
+    void retrieveFoodById_foodNotFound() {
+        // Arrange
+        User user = new User();
+        when(foodRepository.findById(99L)).thenReturn(Optional.empty());
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.retrieveFoodById(user, 99L))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void retrieveFoodById_foodNotOwned() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        User owner = mock(User.class);
+        when(owner.getId()).thenReturn(2L);
+
+        Food existingFood = new Food();
+        existingFood.setUser(owner);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.retrieveFoodById(user, 99L))
+                .isInstanceOf(ResourceNotOwnedException.class);
+    }
+
+    @Test
+    void retrieveFoodByTextSearch_happyFlow() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        Food food1 = new Food();
+        Food food2 = new Food();
+
+        FoodResponse response1 = defaultFoodResponse();
+        FoodResponse response2 = alternateFoodResponse();
+
+        when(foodRepository.findAllByUserAndTextSearch(1L, "text"))
+                .thenReturn(List.of(food1, food2));
+        when(foodMapper.generateResponse(food1)).thenReturn(response1);
+        when(foodMapper.generateResponse(food2)).thenReturn(response2);
+
+        List<FoodResponse> expectedResponse = List.of(response1, response2);
+
+        // Act
+        List<FoodResponse> actualResponse = foodService.retrieveFoodsByTextSearch(user, "text");
+
+        // Assert
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+        verify(foodRepository).findAllByUserAndTextSearch(1L, "text");
+    }
+
+    @Test
+    void retrieveAllFoods_happyFlow() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        Food food1 = new Food();
+        Food food2 = new Food();
+
+        FoodResponse response1 = defaultFoodResponse();
+        FoodResponse response2 = alternateFoodResponse();
+
+        when(foodRepository.findAllByUserId(1L)).thenReturn(List.of(food1, food2));
+        when(foodMapper.generateResponse(food1)).thenReturn(response1);
+        when(foodMapper.generateResponse(food2)).thenReturn(response2);
+
+        List<FoodResponse> expectedResponse = List.of(response1, response2);
+
+        // Act
+        List<FoodResponse> actualResponse = foodService.retrieveAllFoods(user);
+
+        // Assert
+        assertThat(actualResponse).isEqualTo(expectedResponse);
+        verify(foodRepository).findAllByUserId(1L);
+    }
+
+    @Test
+    void deleteFoodById_happyFlow() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        Food existingFood = new Food();
+        existingFood.setUser(user);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        // Act
+        foodService.deleteFoodById(user, 99L);
+
+        // Assert
+        verify(foodRepository).delete(existingFood);
+    }
+
+    @Test
+    void deleteFoodById_foodNotFound() {
+        // Arrange
+        User user = new User();
+        when(foodRepository.findById(99L)).thenReturn(Optional.empty());
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.deleteFoodById(user, 99L))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
+    @Test
+    void deleteFoodById_foodNotOwned() {
+        // Arrange
+        User user = mock(User.class);
+        when(user.getId()).thenReturn(1L);
+
+        User owner = mock(User.class);
+        when(owner.getId()).thenReturn(2L);
+
+        Food existingFood = new Food();
+        existingFood.setUser(owner);
+        when(foodRepository.findById(99L)).thenReturn(Optional.of(existingFood));
+
+        // Act + Assert
+        assertThatThrownBy(() -> foodService.deleteFoodById(user, 99L))
+                .isInstanceOf(ResourceNotOwnedException.class);
+    }
+}
