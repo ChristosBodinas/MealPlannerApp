73602c99e5a3139eac781fd22b623ea0f7dc9a2e
ChristosBodinas <94917338+ChristosBodinas@users.noreply.github.com>
2026-08-03 19:48:21 +0000

    FIX: Added missing Sex and Gender enums.

---
+++ src/main/java/org/example/mealplannerapp/common/Gender.java
@@ -0,0 +1,8 @@
+package org.example.mealplannerapp.constants;
+
+// TODO: Add proper names and possibly shorten the values.
+public enum Gender {
+    MALE,
+    FEMALE,
+    NON_BINARY
+}
---
+++ src/main/java/org/example/mealplannerapp/common/Sex.java
@@ -0,0 +1,7 @@
+package org.example.mealplannerapp.constants;
+
+// TODO: Add proper names.
+public enum Sex {
+    MALE,
+    FEMALE
+}
