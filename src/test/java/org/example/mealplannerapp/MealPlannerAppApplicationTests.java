d3243737fc5f1adc01f8f284e35be3763ed928f8
ChristosBodinas <chr.bodinas@gmail.com>
2026-08-21 17:51:18 +0300

    WIP Emptied out branch.

--- src/test/java/org/example/mealplannerapp/MealPlannerAppApplicationTests.java
+++ src/test/java/org/example/mealplannerapp/MealPlannerAppApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class MealPlannerAppApplicationTests {
 
